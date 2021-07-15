d82990343a4408662635fe318686b1e5883a102d
masonasons <darkflier01@gmail.com>
2021-07-15 11:32:09 -0500

    build files for windows

--- copy.bat
+++ copy.bat
@@ -1,11 +1,9 @@
 rmdir /S /Q windist
 mkdir windist
-xcopy /Q /E c:\tempbuild\quinter.dist windist /Y
+xcopy /Q /E c:\tempbuild\QPlay.dist windist /Y
 xcopy /Q /E docs windist /Y
 xcopy /Q *.dll windist /Y
-xcopy /Q /E ..\quinterfiles windist /S /Y
-mkdir windist\sounds
-xcopy /Q /E sounds windist\sounds /Y
+xcopy /Q /E ..\QPlayfiles windist /S /Y
 rmdir /S /Q c:\tempbuild
 del windist\t*86t.dll
 pause
